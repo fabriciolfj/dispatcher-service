8c00a74c043c9c3e13e4d5837d8b3dbdae8a2627
fabricio <fabricio.jacob@outlook.com>
2022-05-03 21:23:15 -0300

    add

---
+++ src/test/java/com/github/fabriciolfj/dispatcherservice/DispatcherServiceApplicationTests.java
@@ -0,0 +1,31 @@
+package com.github.fabriciolfj.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.function.Function;
+
+@FunctionalSpringBootTest
+class DispatcherServiceApplicationTests {
+
+	@Autowired
+	private FunctionCatalog catalog;
+
+	@Test
+	void packAndLabelOrder() {
+		Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel = catalog.lookup(Function.class, "pack|label");
+
+		long orderId = 121;
+
+		StepVerifier
+				.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
+				.expectNextMatches(dispatcher -> dispatcher.equals(new OrderDispatchedMessage(orderId)))
+				.verifyComplete();
+	}
+
+}
